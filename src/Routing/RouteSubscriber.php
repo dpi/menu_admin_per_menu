ee72273e70169d95d3c988c393d6f2c522364034
git <git@3308753.no-reply.drupal.org>
2016-08-16 14:20:04 +0200

    Issue #2544184 by mkdok, vorapoap: Port to drupal 8

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -18,9 +18,31 @@ class RouteSubscriber extends RouteSubscriberBase {
   /**
    * {@inheritdoc}
    */
-  public function alterRoutes(RouteCollection $collection) {
-    if ($route = $collection->get('entity.menu.collection')) {
-      $route->setRequirements(['_custom_access' => '_menu_admin_per_menu_access']);
+  protected function alterRoutes(RouteCollection $collection) {
+    $routes = $collection->all();
+    foreach ($routes as $route_name => $route) {
+      switch ($route_name) {
+        case 'entity.menu.collection':
+          $route->setDefaults(['_controller' => '\Drupal\menu_admin_per_menu\Controller\MenuAdminPerMenuController::menuOverviewPage']);
+          $route->setRequirements(['_custom_access' => '\Drupal\menu_admin_per_menu\Access\MenuAdminPerMenuAccess::menusOverviewAccess']);
+          break;
+
+        case 'entity.menu.edit_form':
+        case 'entity.menu.add_link_form':
+          $route->setRequirements(['_custom_access' => '\Drupal\menu_admin_per_menu\Access\MenuAdminPerMenuAccess::menuAccess']);
+          break;
+
+        case 'menu_ui.link_edit':
+        case 'menu_ui.link_reset':
+          $route->setRequirements(['_custom_access' => '\Drupal\menu_admin_per_menu\Access\MenuAdminPerMenuAccess::menuLinkAccess']);
+          break;
+
+        case 'entity.menu_link_content.canonical':
+        case 'entity.menu_link_content.delete_form':
+          $route->setRequirements(['_custom_access' => '\Drupal\menu_admin_per_menu\Access\MenuAdminPerMenuAccess::menuItemAccess']);
+          break;
+
+      }
     }
   }
 
