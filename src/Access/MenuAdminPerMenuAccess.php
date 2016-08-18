a1eb5b3fb8981747e42fc62c6e80f8379a05be72
mkdok <mkdok@3308753.no-reply.drupal.org>
2016-08-18 17:23:35 +0200

    Issue #2784877 by mkdok: Keep D8 in sync with D7 before publishing a first stable release

--- src/Access/MenuAdminPerMenuAccess.php
+++ src/Access/MenuAdminPerMenuAccess.php
@@ -9,103 +9,84 @@ namespace Drupal\menu_admin_per_menu\Access;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Menu\MenuLinkInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\system\Entity\Menu;
+use Drupal\menu_admin_per_menu\MenuAdminPerMenuAccessInterface;
 use Drupal\menu_link_content\Entity\MenuLinkContent;
+use Drupal\system\Entity\Menu;
 
 /**
  * Checks access for displaying administer menu pages.
  */
-class MenuAdminPerMenuAccess {
+class MenuAdminPerMenuAccess implements MenuAdminPerMenuAccessInterface {
 
   /**
-   * Return array of all specific menu permissions.
+   * {@inheritdoc}
    */
-  public static function menu_admin_per_menu_permissions() {
-    $permissions = [];
-    $menus = menu_ui_get_menus();
-    foreach ($menus as $name => $title) {
-      $permissions[] = 'administer ' . $name . ' menu items';
+  public function getPerMenuPermissions(AccountInterface $account) {
+    $perms_menu = &drupal_static(__FUNCTION__, array());
+
+    if (!$perms_menu) {
+      $menus = menu_ui_get_menus();
+      foreach ($menus as $name => $title) {
+        $permission = 'administer ' . $name . ' menu items';
+        if ($account->hasPermission($permission)) {
+          $perms_menu[$permission] = $permission;
+        }
+      }
+      \Drupal::moduleHandler()->alter('menu_admin_per_menu_get_permissions', $perms_menu, $account);
     }
-    return $permissions;
+
+
+    return $perms_menu;
   }
 
   /**
-   * A custom access check for menu overview page
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   *
-   * @return \Drupal\Core\Access\AccessResult
-   *   The access result.
+   * {@inheritdoc}
    */
   public function menusOverviewAccess(AccountInterface $account) {
     if ($account->hasPermission('administer menu')) {
       return AccessResult::allowed();
     }
-    $permissions = $this::menu_admin_per_menu_permissions();
-    foreach ($permissions as $permission) {
-      if ($account->hasPermission($permission)) {
-        return AccessResult::allowed();
-      }
+    $permissions = $this::getPerMenuPermissions($account);
+    if ($account->hasPermission('administer menu') || $permissions) {
+      return AccessResult::allowed();
     }
     return AccessResult::neutral();
   }
 
   /**
-   * A custom access check for menu page and add link page.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   * @param \Drupal\system\Entity\Menu $menu
-   *   Run access checks for this menu object.
-   *
-   * @return \Drupal\Core\Access\AccessResult
-   *   The access result.
+   * {@inheritdoc}
    */
   public function menuAccess(AccountInterface $account, Menu $menu) {
-    $permission = 'administer '. $menu->get('id') .' menu items';
+    $permission = 'administer ' . $menu->get('id') . ' menu items';
+    $permissions = $this::getPerMenuPermissions($account);
     if ($account->hasPermission('administer menu')
-      || $account->hasPermission($permission)) {
+      || isset($permissions[$permission])) {
       return AccessResult::allowed();
     }
     return AccessResult::neutral();
   }
 
   /**
-   * A custom access check for menu items page.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   * @param \Drupal\menu_link_content\Entity\MenuLinkContent $menu_link_content
-   *   Run access checks for this menu item object.
-   *
-   * @return \Drupal\Core\Access\AccessResult
-   *   The access result.
+   * {@inheritdoc}
    */
   public function menuItemAccess(AccountInterface $account, MenuLinkContent $menu_link_content = NULL) {
-    $permission = 'administer '. $menu_link_content->getMenuName() .' menu items';
+    $permission = 'administer ' . $menu_link_content->getMenuName() . ' menu items';
+    $permissions = $this::getPerMenuPermissions($account);
     if ($account->hasPermission('administer menu')
-      || $account->hasPermission($permission)) {
+      || isset($permissions[$permission])) {
       return AccessResult::allowed();
     }
     return AccessResult::neutral();
   }
 
   /**
-   * A custom access check for menu link page.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   * @param \Drupal\Core\Menu\MenuLinkInterface $menu_link_plugin
-   *   Run access checks for this menu link object.
-   *
-   * @return \Drupal\Core\Access\AccessResult
-   *   The access result.
+   * {@inheritdoc}
    */
   public function menuLinkAccess(AccountInterface $account, MenuLinkInterface $menu_link_plugin = NULL) {
-    $permission = 'administer '. $menu_link_plugin->getMenuName() .' menu items';
+    $permission = 'administer ' . $menu_link_plugin->getMenuName() . ' menu items';
+    $permissions = $this::getPerMenuPermissions($account);
     if ($account->hasPermission('administer menu')
-      || $account->hasPermission($permission)) {
+      || isset($permissions[$permission])) {
       return AccessResult::allowed();
     }
     return AccessResult::neutral();
--- src/Controller/MenuAdminPerMenuController.php
+++ src/Controller/MenuAdminPerMenuController.php
@@ -52,17 +52,19 @@ class MenuAdminPerMenuController extends ControllerBase {
     if ($account->hasPermission('administer menu')) {
       return $menu_table;
     }
+    $allowedMenusService = \Drupal::service('menu_admin_per_menu.allowed_menus');
+    $allowed_menus = $allowedMenusService->getPerMenuPermissions($account);
     foreach ($menu_table['table']['#rows'] as $menu_key => $menu_item) {
-      if (!$account->hasPermission("administer $menu_key menu items")) {
+      if (!isset($allowed_menus["administer $menu_key menu items"])) {
         unset($menu_table['table']['#rows'][$menu_key]);
       }
       else {
         $menu_row = &$menu_table['table']['#rows'][$menu_key];
         $menu_operations = &$menu_row['operations']['data']['#links'];
         $menu_operations['list']['title'] = t('List links');
-        $menu_operations['list']['url'] = Url::fromRoute('entity.menu.edit_form', array('menu' => $menu_key));;
+        $menu_operations['list']['url'] = Url::fromRoute('entity.menu.edit_form', array('menu' => $menu_key));
         $menu_operations['add']['title'] = t('Add link');
-        $menu_operations['add']['url'] = Url::fromRoute('entity.menu.add_link_form', array('menu' => $menu_key));;
+        $menu_operations['add']['url'] = Url::fromRoute('entity.menu.add_link_form', array('menu' => $menu_key));
       }
     }
     return $menu_table;
